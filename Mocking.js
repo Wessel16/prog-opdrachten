4d4caa37a88d5c611996306f2dcb5d84c24a2fbc
Wessel van Arkel <36850@ma-web.nl>
2023-11-03 13:06:16 +0100

    verbeterde versie Mocking

--- Mocking.js
+++ Mocking.js
@@ -1,5 +1,16 @@
-let myArr = ["Ik houd van pindakaas", "iK HoUd vAn pInDaKaAs"]; 
-for (let i = 0; i < myArr.length; i++) {
-    const element = myArr[i];
-    console.log (element)
-}
+let rl = require('readline-sync')
+let vraag = rl.question("Zeg iets.") 
+
+let vraag2 = ""
+for (let i = 0; i < vraag.length; i++) {
+    let element = vraag[i];
+    if(i % 2 == 0){
+    element = element.toUpperCase()
+    }
+    vraag2 += element
+    }
+    console.log(vraag2)
+    
+
+
+
